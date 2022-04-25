25668dc356942b591cd8c2b5ba3de879c9a21c3d
KQT3 <kristoffer.pettersson@sensera.se>
2022-04-25 18:51:27 +0200

    first commit

---
+++ src/main/java/com/example/keycloakauthbackend/KeycloakAuthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloakauthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakAuthBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakAuthBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keycloakauthbackend/KeycloakAuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloakauthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAuthBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
